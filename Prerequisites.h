d60a10e3bf7c06eb162eaf49edd8878ff72d3261
Darkwarrior0417 <arturosf17@gmail.com>
2024-10-03 07:56:40 -0600

    Agregar archivos de proyecto.

---
+++ Prerequisites.h
@@ -0,0 +1,43 @@
+#pragma once
+
+// Librerias STD
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <thread>
+
+// Third Parties
+#include <SFML/Graphics.hpp>
+#include "TSharedPointer.h"
+#include "TWeakPointer.h"
+#include "TStaticPtr.h"
+#include "TUniquePtr.h"
+// Enums
+enum
+    ShapeType {
+    EMPTY = 0,
+    CIRCLE = 1,
+    RECTANGLE = 2,
+    TRIANGLE = 3
+};
+
+// MACRO for safe release of resources
+#define SAFE_PTR_RELEASE(x) if(x != nullptr) { delete x; x = nullptr; }
+
+#define MESSAGE(classObj, method, state)                      \
+{                                                             \
+    std::ostringstream os_;                                   \
+    os_ << classObj << "::" << method << " : "                \
+        << "[CREATION OF RESOURCE" << ": " << state "] \n";\
+    std::cerr << os_.str();                                   \
+}
+
+#define ERROR(classObj, method, errorMSG)                         \
+{                                                                 \
+    std::ostringstream os_;                                       \
+    os_ << "ERROR : " << classObj << "::" << method << " : "      \
+        << "  Error in data from params [" << errorMSG"] \n"; \
+    std::cerr << os_.str();                                       \
+    exit(1);                                                      \
+}
